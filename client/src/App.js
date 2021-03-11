cb308b7039d50f6c1a1b2aa38ca57928c8a894f2
Cayman Heng <hengcayman@gmail.com>
2021-03-11 09:56:25 -0500

    added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Search from "./pages/Search/Search";
 import Save from "./pages/Save/Save";
+import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Search />
       <Save />
     </div>
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Google Books
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="/">
+              Search
+            </a>
+            <a className="nav-link" href="/save">
+              Save
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

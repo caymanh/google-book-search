574b17ae6a294acc74965a097a31695b79ec6229
Cayman Heng <hengcayman@gmail.com>
2021-03-16 15:46:36 -0400

    updated Jumbotron component

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Jumbotron.css";
 
-function Jumbotron({ children }) {
-  return <div className="jumbotron mt-4">{children}</div>;
+function Jumbotron() {
+  return (
+    <div className="container my-4">
+      <div className="row my-5 my-4">
+        <div className="col-12">
+          <img
+            src="../../../img/google-books-logo.png"
+            className="img-fluid center"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Jumbotron;
